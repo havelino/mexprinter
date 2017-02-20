e6ab1b77fdcefafdf02928818b7a414de7d7777a
Hugo A H <Hugo A H>
2017-02-19 20:15:40 -0600

    changues with index, contact

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
+
+//general libraries
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var nunjucks  = require('nunjucks');
 
+//internal app libraries
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'pug');
+//config view engine with Nunjucks setup
+nunjucks.configure(__dirname + "/views", {
+	autoescape: true,
+	express: app
+});
+
+app.set('view engine', 'nunjucks');
+//static files exposed to client side
+// http://localhost:3000/public/stylesheets/...
+// http://localhost:3000/public/javascript/...
+// http://localhost:3000/public/images/...
+
+app.use('/public',express.static(path.join(__dirname, 'public')))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -40,7 +57,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error.html');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.render('index.html', {
+        title : "Mexprint3r Mexico"
+        });
+});
+
+/* GET home page. */
+router.get('/index', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.render('index.html', {
+        title : "Mexprint3r Mexico"
+        });
+});
+
+
+router.get('/services', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+
+  res.render('services.html', {
+        title : "Mexprint3r Mexico"
+        });
+});
+
+router.get('/about', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+
+  res.render('about.html', {
+        title : "Mexprint3r Mexico"
+        });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact.html', {
+        title : "Mexprint3r Mexico"
+        });
+});
+
+router.get('/slicer', function(req, res, next) {
+  res.render('slicer.html', {
+        title : "Mexprint3r Mexico"
+        });
 });
 
 module.exports = router;
